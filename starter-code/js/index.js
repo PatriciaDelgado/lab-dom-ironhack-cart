dbdb80cae2f6e88bb998011d2a57e12107aa3800
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-02-12 19:57:26 +0100

    html && first iteration fixed

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,9 @@
+/* Crear un evento clic para el botón Calcular precios
+Este evento ejecutará una función que:
+Recupera el precio unitario del producto.
+Recupera la cantidad de artículos deseados.
+Calcula el precio total en base a estos datos.
+Actualiza el precio total en el DOM */
 function deleteItem(e){
 
 }
@@ -11,7 +17,10 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  var ItemPrice = document.querySelectorAll('.ItemPrice')[0].textContent = "25.00";
+  var ItemsQuanty = document.getElementById("quantity").value;
+  var TotalPrice = document.querySelectorAll('.ItemTotalPrice')[0].textContent = ItemsQuanty * ItemPrice
+  console.log(TotalPrice);
 }
 
 function createQuantityInput(){
@@ -50,3 +59,20 @@ window.onload = function(){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+
+
+
+
+
+var CalcTotalPrices = document.getElementById("calc-prices-buttonn");
+var total = document.createTextNode("hola");
+CalcTotalPrices.appendChild(total);
+
+/* var CalcTotalPrices = document.getElementById("calc-prices-buttonn");
+CalcTotalPrices.onclick = function(){
+  console.log("adding an element to the list");
+} 
+
+var prueba = document.getElementById('calc-prices-buttonn');
+prueba.innerHTML = "hola"; */
